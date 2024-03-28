3d1eb060a201abd6f2a30d7d9736df3bc52be5ae
LenZuro <126126939+LenZuro@users.noreply.github.com>
2024-03-27 18:05:51 -0700

    Header added in its first iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Hero from "./componets/Hero";
+import Highlights from "./componets/Highlights";
+import Navbar from "./componets/Navbar";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 };
 
 export default App;
